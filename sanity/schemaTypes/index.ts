3f9b9f82448104fb063be3077a7902f2b65cff98
ishnoor singh <m22aie233@iitj.ac.in>
2026-05-23 11:06:22 +0530

    removed extra content types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,8 @@
 import { SchemaTypeDefinition } from 'sanity'
 import news from './news'
-import author from './author'
-import program from './program'
 import job from './job'
 import teamMember from './teamMember'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [news, author, program, job, teamMember],
+  types: [news, job, teamMember],
 }
